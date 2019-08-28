66d2976dfebf647c1b33000f9a6b1ca6c430dc4b
AnaMartinez <amonmtz009@gmail.com>
2019-08-28 10:47:14 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var age = prompt("Ingresa tu edad");
+console.log(age);
+var year = 31536000;
+console.log("Tu edad en segundos es " + age*31536000);
+document.write("Tu edad en segundos es " + age*year);
